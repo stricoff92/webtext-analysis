f40f31e3c711f869b666a82c7e74384e638c03a9
Jon <stricoff92@gmail.com>
2021-03-16 18:20:32 -0400

    init angular app

--- webtext/webtext/settings.py
+++ webtext/webtext/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 """
 
 from pathlib import Path
+import os.path
 
 try:
     from webtext import applocals
@@ -148,3 +149,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
+
+
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, "website/templates/static"),
+    os.path.join(BASE_DIR, "website/templates/angular_static"),
+]
