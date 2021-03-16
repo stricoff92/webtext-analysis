0ba4d5161f5bd83bda313b12b1a4503ec3772add
Jon <stricoff92@gmail.com>
2021-03-16 16:48:44 -0400

    added django project

---
+++ webtext/webtext/test_settings.py
@@ -0,0 +1,27 @@
+
+import os.path
+
+from .settings import *
+
+
+ENV = "TESTING"
+DEBUG = True
+SESSION_COOKIE_SECURE = False
+CSRF_COOKIE_SECURE = False
+AUTH_PASSWORD_VALIDATORS = []
+
+
+# Faster insecure hashing for testing only
+PASSWORD_HASHERS = [
+    'django.contrib.auth.hashers.MD5PasswordHasher',
+]
+
+# File Based DB for Faster setup and teardown
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+    }
+}
+
+EMAIL_BACKEND = 'django.core.mail.backends.dummy.EmailBackend'
